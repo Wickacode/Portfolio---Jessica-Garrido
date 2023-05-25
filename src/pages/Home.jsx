fe57b41d59b75b5340977f3c0973c1bd43f70c8a
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-05-25 17:05:27 +0200

    ajustement home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ProfilePicture from "../assets/img/Home/home-picture1.svg";
+import ProfilePicture from "../assets/img/Home/home-picture1.jpg";
 import ProfilePicture2 from "../assets/img/Home/home-picture2.svg";
 
 export default function Home() {
@@ -26,27 +26,33 @@ export default function Home() {
         <div
           className={`profile-pictures ${isActive ? "grayscale-active" : ""}`}
         >
-          <img className="grayscale" src={ProfilePicture} alt="" />
-          <img className="grayscale" src={ProfilePicture2} alt="" />
+          <img className="grayscale homePicture" src={ProfilePicture} alt="" />
+          <img className="grayscale homePicture" src={ProfilePicture2} alt="" />
         </div>
       </div>
-      
+
       <div className="profile-description">
         <div className="title-and-citation">
-          <h2>Développeuse Web Junior</h2>
+          <h2><b>Développeuse Web Junior</b></h2>
           <div>
             <p className="citation">{citationParts[0]}</p>
             <p className="author">{citationParts[1]}</p>
           </div>
         </div>
 
-        <p>
-          Bienvenus sur mon site en ligne ! <br />
+        <p className="home-description">
+          <b>Bienvenus sur mon site en ligne ! </b>
+          <br />
           Passionée par le code et à l'affût des nouvelles technologies, <br />
-          je suis à la recherche d'une entreprise pour y faire mon alternance en
-          Concepcion d'applications web et mobiles. <br />
-          Je vous remercie de votre présence sur mon site, et vous invite à le
-          parcourir pour mieux me découvrir ! <br />
+          J'ai récemment ouvert ma micro-entreprise pour me lancer en tant que
+          développeuse freelance . Mon entreprise répond au doux nom de CODEPIC.
+          Disponible pour tout type de challenge, je suis également à la
+          recherche d'une entreprise pour y faire mon alternance en Concepcion
+          d'applications web et mobiles. <br />
+          Cet apprentissage se déroulera en compagnie de la Fabrique Numérique
+          Paloise. <br />
+          Je vous remercie de votre présence sur mon site, et vous invite à
+          le parcourir pour mieux me découvrir ! <br />
           Au plaisir d'échanger avec vous !
         </p>
       </div>
